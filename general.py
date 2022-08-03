f395398ffc84e80b9e3069169b77fc29f9636486
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-03 19:04:50 +0430

    Adding initial docstring

---
+++ general.py
@@ -0,0 +1,7 @@
+"""
+    General Regular Expressions
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~
+    A collection of general regular expressions.
+    All of these are based on the Python re module.
+    Author: Milad Sadeghi DM - EverLookNeverSee@GitHub
+"""
