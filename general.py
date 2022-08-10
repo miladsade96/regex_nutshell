96fd5958eb7a5da52b0bcab0db1a214e11077bfa
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-10 17:26:28 +0430

    Adding example 23

--- general.py
+++ general.py
@@ -286,3 +286,14 @@ for match in matches:
 
 print("*" * 65)
 # ----------------------------------------------------------------------------------
+
+# Example 23: Negate all lowercase and uppercase alphabet characters.
+print("Example 23: Negate all lowercase and uppercase alphabet characters.")
+pattern = re.compile(r'[^a-zA-Z]')
+matches = pattern.finditer(text_to_search)
+for match in matches:
+    print(f"Match span indexes: {match.span()},   "
+          f"Found characters in text: {text_to_search[match.start():match.end()]}")
+
+print("*" * 65)
+# ----------------------------------------------------------------------------------
